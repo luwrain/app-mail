e2bf6f3bc6fbe72db6db5b8f0148ae05c09c9127
Michael Pozhidaev <msp@luwrain.org>
2020-12-27 15:16:00 +0700

    Using AppBase.onInputEvent()

--- src/main/java/org/luwrain/app/mail/App.java
+++ src/main/java/org/luwrain/app/mail/App.java
@@ -46,28 +46,6 @@ final class App extends AppBase<Strings> implements MonoApp
 	return true;
     }
 
-                boolean onInputEvent(Area area, InputEvent event, Runnable closing)
-    {
-	NullCheck.notNull(area, "area");
-	if (event.isSpecial())
-	    switch(event.getSpecial())
-	    {
-	    case ESCAPE:
-		if (closing != null)
-		    closing.run(); else
-		    closeApp();
-		return true;
-	    }
-	return super.onInputEvent(area, event);
-    }
-
-    @Override public boolean onInputEvent(Area area, InputEvent event)
-    {
-	NullCheck.notNull(area, "area");
-	NullCheck.notNull(event, "event");
-	return onInputEvent(area, event, null);
-    }
-
     MailStoring getMailStoring()
     {
 	return this.storing;
